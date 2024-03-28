361a06dbfb7eff34d85a37f06185722217c21ce3
ZeroZ9 <tranlonguuu@gmail.com>
2024-03-28 17:59:34 -0400

    finish setup folder for project

---
+++ ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by Long Tran M2 on 28/3/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
